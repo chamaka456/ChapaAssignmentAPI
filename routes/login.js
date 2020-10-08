1287734cc0751fab0e3ac539b378afe6a08d11f9
dinith72 <jdinith72@gmail.com>
2020-10-08 08:22:51 +0530

    basic db connection through mysql

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const conn = require('../services/DbConnection');
 const Joi = require('joi');
 const router = express.Router();
 
 
 
-router.get('/user', (req , res)=> {
+router.post('/user', (req , res)=> {
+       
+    const userName = req.body.userName;
+    const password = req.body.Password;
     const schema = Joi.object({
         userName : Joi.string().required(),
         password: Joi.string().required()
@@ -13,10 +17,39 @@ router.get('/user', (req , res)=> {
     const {error , value } = schema.validate(req.body);
     if(error){
         res.status(400).send(error.details[0].message);
+        return null;
     }
-    else {
-        res.send('welocome to login');
-    }
+    
+    const query = `select * from Users where username like ?`;
+    conn.connect(err => {
+        if(!err){
+            conn.query(query,[userName] ,(err , rows )=> {
+                if(!err){
+                    console.log('db data' + rows);
+                    if(rows[0].password === password){
+                        res.send({
+                            status: "Success"
+                        });
+                    }
+                    else {
+                        res.send({
+                            status: "Error"
+                        })
+                    }
+                     
+                }
+                else {
+                    console.log('err' + err);
+                }
+            });
+            conn.end();
+        }
+    });
+
+    
+    
+  
+    
    
 })
 
---
+++ services/DbConnection.js
@@ -0,0 +1,21 @@
+
+const mysql = require('mysql');
+
+
+const connection = mysql.createConnection({
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b278c1fc72d411',
+    password: '610a2736',
+    database: 'heroku_6b6f34f15decd64'
+  });
+
+
+ 
+  
+  
+   
+ 
+ 
+
+
+  module.exports = connection;
